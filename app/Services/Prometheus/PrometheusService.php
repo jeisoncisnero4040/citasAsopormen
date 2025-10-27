3789176a55c774b4cd01c533e6ac35eecb97e987
jeisoncinsero4040 <legoladhojavergmail.com>
2025-10-27 15:06:28 -0500

    deploy new server

---
+++ app/Services/Prometheus/PrometheusService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Prometheus;
+
+use Prometheus\CollectorRegistry;
+use Prometheus\RenderTextFormat;
+
+class PrometheusService
+{
+    private CollectorRegistry $collectorRegistry;
+
+    public function __construct ( CollectorRegistry $registry )
+    {
+        $this->collectorRegistry = $registry->getDefault();
+    }
+
+    public function metrics (): string
+    {
+        $renderer = new RenderTextFormat();
+
+        $result = $renderer->render( $this->collectorRegistry->getMetricFamilySamples() );
+
+        header( 'Content-type: ' . RenderTextFormat::MIME_TYPE );
+
+        return $result;
+
+    }
+
+}
